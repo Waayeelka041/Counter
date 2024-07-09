d9f8bfe798495ffd16ddd3e8e83ccd81673ef70c
Waayeelka041 <bosswaayelka@gmailcom>
2024-07-09 09:47:50 +0300

    Responsive counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -24,13 +24,13 @@ function Counter(){
     }
     return <div className="flex justify-center">
 <div>
-    <div counter  className="w-[350px] h-[100px] rounded-full text-center text-4xl font-bold border-2 border-black ml-16 mt-5">
+    <div counter  className="w-[350px] h-[100px] rounded-full text-center text-4xl font-bold border-2 border-black ml-4 sm:ml-16 mt-20">
     <h1>{Tiriye} </h1>
          </div>
-    <button   onClick={increment}  className="bg-orange-600 text-3xl px-3 py-2 m-3 ml-20   rounded">increment</button>
+    <button   onClick={increment}  className="bg-orange-600 text-3xl px-3 py-2 m-3 sm:ml-20   rounded">increment</button>
     <button  onClick={Decrement} className="bg-orange-600 text-3xl px-3 py-2 m-3 rounded">Decrement</button>
-    <div className="ml-[200px]">
-    <button  onClick={Reset} className="bg-orange-600 text-3xl px-3 py-2 m-3 rounded">Reset</button>
+    <div className="ml-[110px]  sm:ml-[200px]">
+    <button  onClick={Reset} className="bg-orange-600 text-3xl px-3 py-2 m-3  rounded">Reset</button>
     </div>
 </div>
     </div>
